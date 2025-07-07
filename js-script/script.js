2abbed6be2ca3fd6a120a87827e098844042e381
codersimone <97359905+codersimone@users.noreply.github.com>
2025-07-07 19:19:35 +0300

    Added the creation of a header and container library of book objects, as well as the function of displaying book cards through the DOM.

--- js-script/script.js
+++ js-script/script.js
@@ -7,10 +7,10 @@ function Book(title, author, pages, isRead) {
     this.pages = pages;
     this.isRead = isRead;
 
-    this.info = function () {
-        let readStatus = this.isRead ? 'already read' : 'not read yet';
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
-    };
+    // this.info = function () {
+    //     let readStatus = this.isRead ? 'already read' : 'not read yet';
+    //     return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+    // };
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
@@ -28,3 +28,34 @@ mainContainer.appendChild(libraryHeader);
 const libraryContainer = document.createElement('div');
 libraryContainer.classList.add('library-container');
 mainContainer.appendChild(libraryContainer);
+
+function displayBooks() {
+    const container = document.querySelector('.library-container');
+    container.innerHTML = '';
+
+    myLibrary.forEach((book) => {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
+        bookCard.innerHTML = `
+            <h3>${book.title}</h3>
+            <p><strong>Author:<strong> ${book.author}</p>
+            <p><strong>Volume:<strong> ${book.pages}</p>
+            <p><strong>Status:<strong> ${
+                book.isRead ? 'Already read' : 'Not read yet'
+            }</p>
+        `;
+
+        container.appendChild(bookCard);
+    });
+}
+
+addBookToLibrary(`The Master and Margarita`, `M. Bulgakov`, 528, true);
+addBookToLibrary(
+    `Harry Potter and the Philosopher's Stone`,
+    `J. K. Rowling`,
+    223,
+    true
+);
+addBookToLibrary(`Dandelion Wine`, `R. Bradbury`, 281, false);
+
+displayBooks();
