65bebdbcfd468ff97402a0a4ff816319a6088652
codersimone <97359905+codersimone@users.noreply.github.com>
2025-07-07 18:08:10 +0300

    Created a storage array and a constructor for creating book objects.

--- js-script/script.js
+++ js-script/script.js
@@ -1,1 +1,14 @@
+const myLibrary = [];
 
+function Book(title, author, pages, isRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+
+    this.info = function () {
+        let readStatus = this.isRead ? 'already read' : 'not read yet';
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+    };
+}
