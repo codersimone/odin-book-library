eb639476509d9991afcf52fe4ff1bcf8e6ea8726
codersimone <97359905+codersimone@users.noreply.github.com>
2025-07-07 23:28:24 +0300

    Changed the structure of book card markup output via DOM in the displayBooks function.

--- script/script.js
+++ script/script.js
@@ -13,44 +13,53 @@ function addBookToLibrary(title, author, pages, isRead) {
     myLibrary.push(newBook);
 }
 
-// const mainContainer = document.querySelector('.main-container');
+function displayBooks() {
+    const container = document.querySelector('.library-container');
 
-// const libraryHeader = document.createElement('h1');
-// libraryHeader.classList.add('library-header');
-// libraryHeader.textContent = 'My library';
-// mainContainer.appendChild(libraryHeader);
+    container.innerHTML = '';
 
-// const libraryContainer = document.createElement('div');
-// libraryContainer.classList.add('library-container');
-// mainContainer.appendChild(libraryContainer);
+    myLibrary.forEach((book) => {
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book-card');
 
-// function displayBooks() {
-//     const container = document.querySelector('.library-container');
-//     container.innerHTML = '';
+        const title = document.createElement('h3');
+        title.classList.add('title');
+        title.textContent = book.title;
 
-//     myLibrary.forEach((book) => {
-//         const bookCard = document.createElement('div');
-//         bookCard.classList.add('book-card');
-//         bookCard.innerHTML = `
-//             <h3>${book.title}</h3>
-//             <p><strong>Author:</strong> ${book.author}</p>
-//             <p><strong>Volume:</strong> ${book.pages}</p>
-//             <p><strong>Status:</strong> ${
-//                 book.isRead ? 'Already read' : 'Not read yet'
-//             }</p>
-//         `;
+        const сardBody = document.createElement('div');
+        сardBody.classList.add('card-body');
 
-//         container.appendChild(bookCard);
-//     });
-// }
+        const author = document.createElement('p');
+        author.classList.add('author');
+        author.innerHTML = `<strong>Author:</strong> ${book.author}`;
 
-// addBookToLibrary(`The Master and Margarita`, `M. Bulgakov`, 528, true);
-// addBookToLibrary(
-//     `Harry Potter and the Philosopher's Stone`,
-//     `J. K. Rowling`,
-//     223,
-//     true
-// );
-// addBookToLibrary(`Dandelion Wine`, `R. Bradbury`, 281, false);
+        const pages = document.createElement('p');
+        pages.classList.add('pages');
+        pages.innerHTML = `<strong>Volume:</strong> ${book.pages} pages`;
 
-// displayBooks();
+        const status = document.createElement('p');
+        status.classList.add('status');
+        status.innerHTML = `<strong>Status:</strong><span> ${
+            book.isRead ? 'Already read' : 'Not read yet'
+        }</span>`;
+
+        bookCard.appendChild(title);
+        bookCard.appendChild(сardBody);
+        bookCard.appendChild(author);
+        bookCard.appendChild(pages);
+        bookCard.appendChild(status);
+
+        container.appendChild(bookCard);
+    });
+}
+
+addBookToLibrary(`The Master and Margarita`, `M. Bulgakov`, 528, true);
+addBookToLibrary(
+    `Harry Potter and the Philosopher's Stone`,
+    `J. K. Rowling`,
+    223,
+    true
+);
+addBookToLibrary(`Dandelion Wine`, `R. Bradbury`, 281, false);
+
+displayBooks();
